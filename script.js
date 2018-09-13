f923c9a2a32065844d9b111efad68ca462c86820
vaibhav bhawalkar <vaibhavbhawalkar98.vb@gmail.com>
2018-09-14 00:27:04 +0530

    added difficulty selector

--- script.js
+++ script.js
@@ -1,126 +1,134 @@
-var tiles=document.getElementsByClassName("block");
-var state=[0,0,0,0,0,0,0,0,0];
-var game=true;
-var visible=false;
-var HUMAN=false;
-var COMPUTER=true;
-var HUMVAL=-1;
-var COMVAL=1;
-var currentPlayer=HUMAN;
-var winMatrix=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-function reset()
-{
-	for(var x=0;x<9;x++)
-	{
-		tiles[x].style.background="#fff";
-		tiles[x].innerHTML="";
-		state[x]=0;
+let tiles
+let state
+let game
+let visible
+let HUMAN
+let COMPUTER
+let HUMVAL
+let COMVAL
+let currentPlayer
+let winMatrix
+let difficulty
+
+function init() {
+	tiles = document.getElementsByClassName("block");
+	state = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+	game = true;
+	visible = false;
+	HUMAN = false;
+	COMPUTER = true;
+	HUMVAL = -1;
+	COMVAL = 1;
+	currentPlayer = HUMAN;
+	winMatrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+	difficulty = document.querySelector('#difficulty-selector').value
+}
+function reset() {
+	for (let x = 0; x < 9; x++) {
+		tiles[x].style.background = "#fff";
+		tiles[x].innerHTML = "";
+		state[x] = 0;
 	}
-	game=true;
+	game = true;
 	document.getElementById("r").innerHTML = "Match Result : NULL";
+	init()
 }
 
-function claim(clicked)
-{
-	if(!game)
+function claim(clicked) {
+	if (!game)
 		return;
-	for(var x=0;x<9;x++)
-	{
-		if(tiles[x]==clicked&&state[x]==0)
-		{
-			set(x,currentPlayer);
+	for (let x = 0; x < 9; x++) {
+		if (tiles[x] == clicked && state[x] == 0) {
+			set(x, currentPlayer);
 			aiturn(state, 0, currentPlayer, true);
 		}
 	}
+	console.log(difficulty)
 }
-function set(index,player)
-{
-	if(!game)
-	return;
-	if(state[index]==0)
-	{
-		tiles[index].style.background=player==HUMAN?"#33a":"#a33";
-		state[index]=player==HUMAN?HUMVAL:COMVAL;
-		currentPlayer=!currentPlayer;
-		aiturn(state,0,currentPlayer,false);
-		if(checkWin(state,player)||checkFull(state))
-		{
-			for(var x=0;x<9;x++)
-			tiles[x].innerHTML="";
-			game=false;
+function set(index, player) {
+	if (!game)
+		return;
+	if (state[index] == 0) {
+		tiles[index].style.background = player == HUMAN ? "#33a" : "#a33";
+		state[index] = player == HUMAN ? HUMVAL : COMVAL;
+		currentPlayer = !currentPlayer;
+		aiturn(state, 0, currentPlayer, false);
+		if (checkWin(state, player) || checkFull(state)) {
+			for (let x = 0; x < 9; x++)
+				tiles[x].innerHTML = "";
+			game = false;
 			isGame();
 		}
 	}
 }
-function checkWin(board,player)
-{
-	var value=player==HUMAN?HUMVAL:COMVAL;
-	for(var x=0;x<8;x++)
-	{
-		var win=true;
-		for(var y=0;y<3;y++)
-		{
-			if(board[winMatrix[x][y]]!=value)
-			{
-				win=false;break;
+function checkWin(board, player) {
+	let value = player == HUMAN ? HUMVAL : COMVAL;
+	for (let x = 0; x < 8; x++) {
+		let win = true;
+		for (let y = 0; y < 3; y++) {
+			if (board[winMatrix[x][y]] != value) {
+				win = false; break;
 			}
 		}
-		if(win)
+		if (win)
 			return true;
 	}
 	return false;
 }
-function checkFull(board)
-{
-	for(var x=0;x<9;x++)
-		if(board[x]==0)
-		return false;
+function checkFull(board) {
+	for (let x = 0; x < 9; x++)
+		if (board[x] == 0)
+			return false;
 	return true;
 }
-function aiturn(board,depth,player,turn)
-{
-	if(checkWin(board,!player))
-		return -10+depth;
-	if(checkFull(board))
+function aiturn(board, depth, player, turn) {
+	if (depth >= difficulty) return 0;
+	if (checkWin(board, !player))
+		return -10 + depth;
+	if (checkFull(board))
 		return 0;
-	var value = player==HUMAN ? HUMVAL : COMVAL;
-	var max = -Infinity;
-	var index=0;
-	for(var x=0;x<9;x++)
-	{
-		if(depth==0){
-			tiles[x].innerHTML="";
+	let value = player == HUMAN ? HUMVAL : COMVAL;
+	let max = -Infinity;
+	let index = 0;
+	for (let x = 0; x < 9; x++) {
+		if (depth == 0) {
+			tiles[x].innerHTML = "";
 		}
-		if(board[x]==0)
-		{
-			var newboard=board.slice();
-			newboard[x]=value;
-			var moveval=-aiturn(newboard,depth+ 1,!player,false);
-			if(depth==0)
-			tiles[x].innerHTML=moveval;
-			if(moveval>max)
-			{
-				max=moveval;
-				index=x;
+		if (board[x] == 0) {
+			let newboard = board.slice();
+			newboard[x] = value;
+			let moveval = -aiturn(newboard, depth + 1, !player, false);
+			if (depth == 0)
+				tiles[x].innerHTML = moveval;
+			if (moveval > max) {
+				max = moveval;
+				index = x;
 			}
 		}
 	}
-	if(turn){
-			set(index,player);
+	if (turn) {
+		set(index, player);
 	}
-	
+
 	return max;
 }
 function isGame() {
-	if(!game){
+	if (!game) {
 
-		var result;
-		if(checkWin(state,!HUMAN)||checkWin(state,HUMAN)){
+		let result;
+		if (checkWin(state, !HUMAN)) {
 			result = "won by computer.";
-		}else{
+		} else if (checkWin(state, HUMAN)) {
+			result = "won by You.";
+		} else {
 			result = "Draw.";
 		}
 		//alert("Match is "+result);
-		document.getElementById("r").innerHTML = "Match is "+result;
+		document.getElementById("r").innerHTML = "Match is " + result;
 	}
 }
+
+init()
+document.querySelector('#difficulty-selector').addEventListener('click', (e) => {
+	difficulty = e.target.value
+})
